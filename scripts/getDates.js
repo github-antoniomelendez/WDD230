49c8ef5e126ea1a30b55593c87a3a73665c7e7a1
tonymelendez1@hotmail.com <tonymelendez1@hotmail.com>
2024-01-20 10:48:40 -0600

    learning activities

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,27 @@
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
 const today1 = document.querySelector('#today1');
 
 const options = {
@@ -46,3 +70,4 @@ function getCelsius(fahrenheit) {
 }
 
 document.querySelector('#temp1').value = `${getCelsius(53).toFixed(1)}°C`;
+
