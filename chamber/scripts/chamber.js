be3c8e8a0a8d8ac4dba10064b244151aaba4885c
tonymelendez1@hotmail.com <tonymelendez1@hotmail.com>
2024-01-20 20:21:57 -0600

    chamber website

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -7,21 +7,6 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
-//dark mode button code
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-	}
-});
 
 // footer and last modification code
 const today1 = document.querySelector('#today1');
