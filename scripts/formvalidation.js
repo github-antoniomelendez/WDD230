1cbc8e34e27f9327aeeb31da7955a30e2b66be81
tonymelendez1@hotmail.com <tonymelendez1@hotmail.com>
2024-02-03 19:41:46 -0600

    team activity improved

--- scripts/formvalidation.js
+++ scripts/formvalidation.js
@@ -1,20 +1,21 @@
 const user1 = document.querySelector("#password");
 const user2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
 
-user2.addEventListener("focusout, checkSame");
+user2.addEventListener("focusout", checkSame);
 
 function checkSame() {
 
     if (user1.value !== user2.value){
         message.textContent = "❗Passwords DO NOT MATCH!";
 		message.style.visibility = "show";
-		kp2.style.backgroundColor = "#fff0f3";
-		kp2.value = "";
-		kp2.focus();
+		user2.style.backgroundColor = "#fff0f3";
+		user2.value = "";
+		user2.focus();
 	} else {
 		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
+		user2.style.backgroundColor = "#fff";
+		user2.style.color = "#000";
 	}
 }
 
