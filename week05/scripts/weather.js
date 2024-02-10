fa3ae7f6343e4e3ca852dec831605672c94e215e
tonymelendez1@hotmail.com <tonymelendez1@hotmail.com>
2024-02-10 12:26:42 -0600

    w05learningactivities

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,7 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=b9391e7aa1a3538c762bfd6297dbde24'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=b9391e7aa1a3538c762bfd6297dbde24'
 
 async function apiFetch() {
     try {
@@ -19,7 +19,7 @@ async function apiFetch() {
 
 //this function displays the results in the HTML document
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;K`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = ` https://openweathermap.org/img/wn/10d@2x.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
